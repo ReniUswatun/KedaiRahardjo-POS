98890ffafdf03c9cf208d17514d4ca68d3046ce8
ReniUswatun <reniuswatunhasanah@student.uns.ac.id>
2025-04-26 10:02:20 +0700

    refactor: update navbar and bottom navigation structure

--- resources/views/customer/dashboard/body/navbar.blade.php
+++ resources/views/customer/dashboard/body/navbar.blade.php
@@ -1,39 +1,16 @@
-<!-- resources/views/components/customer/bottom-nav.blade.php -->
-<div class="fixed bottom-0 left-0 right-0 max-w-xl mx-auto bg-white shadow p-3">
-    <div class="max-w-7xl mx-auto flex justify-around">
-        <a href="{{ route('customer.index') }}"  class="text-center block {{ request()->routeIs('customer.index') ? 'text-red-500' : 'text-gray-700 hover:text-gray-900' }}">
-        <svg class="w-6 h-6 mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" 
-         viewBox="0 0 20 20" aria-hidden="true">
-        <path d="M10.707 1.293a1 1 0 00-1.414 0l-7 7A1 1 0 003 9h1v7a2 2 0 002 2h2a1 1 0 001-1v-4h2v4a1 1 0 001 1h2a2 2 0 002-2V9h1a1 1 0 00.707-1.707l-7-7z" />
-        </svg>
-            Dashboard
+<!-- resources/views/components/customer/navbar.blade.php -->
+<nav class="fixed top-0 left-0 right-0 max-w-xl mx-auto bg-white shadow p-3 z-50">
+    <div class="max-w-7xl mx-auto flex items-start">
+        <!-- Logo/nama aplikasi -->
+        <a href="{{ route('customer.index') }}" class="flex items-center">
+            <img src="{{ asset('assets/images/icon/logo_kedai_rahardjo.svg') }}" alt="Logo" class="h-10 w-10 rounded-full">
+            <div class="ml-2">
+                <p class=" text-xl font-bold text-red-600">
+                Kedai Rahardjo
+            </p>
+                <p class="text-sm text-gray-500">Larangan 01/03, Gayam, Sukoharjo</p>
+            </div>
         </a>
-        <a href="" class="text-gray-700 hover:text-gray-900 text-center">
-            <svg class="w-6 h-6 mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" 
-                 viewBox="0 0 24 24" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3 7h18M3 12h18M3 17h18"></path>
-            </svg>
-            Menus
-        </a>
-        <a href="" class="text-gray-700 hover:text-gray-900 text-center">
-            <svg class="w-6 h-6 mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" 
-                 viewBox="0 0 24 24" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" 
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4"></path>
-                <circle cx="9" cy="21" r="1"></circle>
-                <circle cx="20" cy="21" r="1"></circle>
-            </svg>
-            Carts
-        </a>
-        <a href="" class="text-gray-700 hover:text-gray-900 text-center">
-            <svg class="w-6 h-6 mx-auto mb-1" fill="none" stroke="currentColor" stroke-width="2" 
-       viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round"
-          d="M9 2h6a2 2 0 012 2v2H7V4a2 2 0 012-2zM5 6h14a2 2 0 012 2v12a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2z" />
-                <path stroke-linecap="round" stroke-linejoin="round"
-          d="M9 14l2 2 4-4" />
-            </svg>
-            Orders
-        </a>
+        
     </div>
-</div>
+</nav>
