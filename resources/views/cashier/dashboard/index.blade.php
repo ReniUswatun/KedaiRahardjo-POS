2e2b2e499114dfa15baefd3139066ca1f3e8ce22
dianaalbs <dianaadilla@student.uns.ac.id>
2025-04-27 12:53:33 +0700

    update dashboard kasir

--- resources/views/cashier/dashboard/index.blade.php
+++ resources/views/cashier/dashboard/index.blade.php
@@ -52,35 +52,18 @@
 
 <div class="mx-4 pb-32" x-data="shoppingCart()">
   {{-- Card untuk search --}}
-  <div class="group relative bg-gradient-to-br from-rose-500 to-red-400 rounded-3xl p-6 text-white max-w-xl w-full mx-auto shadow-lg hover:shadow-xl transition-shadow duration-300 overflow-hidden">
-    <!-- Hiasan Lingkaran Blur di Tengah + Animasi Hover -->
-    <div class="absolute top-1/2 left-1/2 w-60 h-60 bg-red-200 bg-opacity-25 rounded-full blur-lg transform -translate-x-full -translate-y-1/2 transition-all duration-500 ease-in-out group-hover:-translate-x-px"></div>
-
-    <h2 class="text-2xl font-bold mb-1 relative z-10">Kedai Rahardjo</h2>
-    <p class="text-sm mb-5 relative z-10">Temukan comforting food mu!</p>
-
-    <div class="relative z-10">
+  <div class="relative z-10">
       <input 
         type="text" 
         placeholder="Cari Makanan" 
         class="w-full pl-5 pr-12 py-3 rounded-full text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-rose-100 focus:ring-offset-2 focus:ring-offset-rose-500 transition-all duration-300 shadow-md"
       />
-      <svg 
-        class="w-5 h-5 text-gray-400 absolute right-4 top-1/2 transform -translate-y-1/2 pointer-events-none" 
-        xmlns="http://www.w3.org/2000/svg" 
-        fill="none" 
-        viewBox="0 0 24 24" 
-        stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-          d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z" />
-      </svg>
     </div>
-  </div>
+
 
 
   {{-- Kategori --}}
   <div class="mt-5" x-data="{ kategoriAktif: '{{ e($categories->first()->slug) }}' }">
-    <h2 class="text-2xl text-red-900 mb-4 font-bold">Menu Kategori</h2>
 
     <!-- Tombol Kategori -->
     <div class="flex flex-wrap justify-evenly mt-2">
