c16f68b9f644c45a144d8d6db9d91b7d40d85679
asyahra19 <asyahrara02@student.uns.ac.id>
2025-04-27 16:36:44 +0700

    update button

--- resources/views/customer/carts/index.blade.php
+++ resources/views/customer/carts/index.blade.php
@@ -25,9 +25,21 @@
                     </div>
 
                     <!-- Tombol Edit mengarahkan ke halaman edit cart -->
-                    <a href="{{ route('customer.menu.show', ['cartId' => $cartId]) }}" class="text-black px-4 py-4 font-semibold">
+                    <div class="flex space-x-2">
+                        <a href="{{ route('customer.menu.show', ['cartId' => $cartId]) }}" class="w-16 py-2 text-green-500 font-medium flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
                             Edit
-                    </a>
+                        </a>
+
+                        <button class="w-16 py-2 text-red-500 font-medium flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                            Hapus   
+                        </button>
+                    </div>
                 </div>
 
 
