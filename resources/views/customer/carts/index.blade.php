3214b160714fb6d0f747943e5c38da25eeb36587
ReniUswatun <reniuswatunhasanah@student.uns.ac.id>
2025-04-27 19:34:50 +0700

    feat: enhance cart functionality and update navbar to display order details

--- resources/views/customer/carts/index.blade.php
+++ resources/views/customer/carts/index.blade.php
@@ -47,13 +47,13 @@
             <div class="bg-white shadow-sm rounded-2xl p-4 mb-4 border border-gray-200">
                 <div class="flex justify-between items-center mb-2">
                     <div class="flex flex-col">
-                        <div class="text-lg font-semibold text-gray-800">Cart ID: {{ $cartId }}</div>
+                        <div class="text-lg font-semibold text-gray-800">Pesanan {{ $loop->iteration }}</div>
                         <span class="text-sm text-gray-500">Total Items: {{ $cart['total_quantity'] }}</span>
                     </div>
 
                     <!-- Tombol Edit mengarahkan ke halaman edit cart -->
                     <div class="flex space-x-2">
-                        <a href="{{ route('customer.menu.show', ['cartId' => $cartId]) }}" class="w-16 py-2 text-green-500 font-medium flex items-center justify-center">
+                        <a href="{{ route('customer.menu.show', ['cartId' => $cartId,'pesanan' => $loop->iteration]) }}" class="w-16 py-2 text-green-500 font-medium flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                             </svg>
